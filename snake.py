f7f977c6e4192925efb75c47097e6cf4d1d51cc0
orahcio <orahcio@gmail.com>
2023-04-18 22:27:50 -0300

    Jogo finalizado, pode aumentar a proporção do snake

--- snake.py
+++ snake.py
@@ -1,4 +1,3 @@
-
 import pygame
 from pygame.locals import *
 from sys import exit
@@ -10,9 +9,15 @@ VERDE = (0,255,0)
 AMARELO = (255,255,0)
 AZUL = (0,0,255)
 FUNDO = (255,255,255)
+PRETO = (0,0,0)
 
 largura = 800
 altura = 640
+
+a = 51
+
+l = int(largura/a)
+h = int(altura/a)
 
 def jogo_na_matrix():
 
@@ -20,14 +25,7 @@ def jogo_na_matrix():
     
     tela = pygame.display.set_mode((largura,altura))
     relogio = pygame.time.Clock()
-
-    # inicialmente a tela está em branco
-    buffer = []
-    for i in range(32):
-        buffer.append([])
-        for j in range(40):
-            buffer[i].append(0)
-
+    fonte = pygame.font.SysFont('Times',35,False,False)
 
     # Controle do snake
     controle = {
@@ -38,20 +36,18 @@ def jogo_na_matrix():
         }
 
     # snake começa no canto superior da tela
-    buffer[0][0] = 1
     snake = [(0,0)]
+    maçã = (int(l/2),int(h/2))
     v = (1,0)
+    comprimento = 6
+
+    pontos = 0
 
     rodando = True
-    alterna = 0
 
     while rodando:
-        relogio.tick(15)
+        relogio.tick(7)
         tela.fill(FUNDO)
-        # apaga matriz
-        for i in range(32):
-            for j in range(40):
-                buffer[i][j] = 0
 
         for evento in pygame.event.get():
             if evento.type == QUIT:
@@ -59,46 +55,49 @@ def jogo_na_matrix():
             if evento.type == KEYDOWN:
                 if evento.key == K_ESCAPE:
                     rodando = False
-                if evento.key == K_w and v[1]<=0:
-                    v = [0,-1]
-                if evento.key == K_s and v[1]>=0:
-                    v = [0,1]
-                if evento.key == K_d and v[0]>=0:
-                    v = [1,0]
-                if evento.key == K_a and v[0]<=0:
-                    v = [-1,0]
-                # if event.key in controle:
-                #     i,j = v
-                #     il,jl = controle[event.key]
-                #     e = i*il+j*jl
-                #     e *= e
-                #     v = (i-il)*e+il, (j-jl)*e+jl
-                #     print(v)
-
+                if evento.key in controle:
+                    # A sequencia de ifs pode ser substituída por essa sequência
+                    i,j = snake[-1][0]-snake[-2][0],snake[-1][1]-snake[-2][1]
+                    il,jl = controle[evento.key]
+                    e = i*il+j*jl
+                    e *= e
+                    v = (i-il)*e+il, (j-jl)*e+jl
 
         # preenche o próximo passo
-        x = (v[0]+snake[-1][0])%40
-        y = (v[1]+snake[-1][1])%32
-        snake.append((x,y))
-        # alterna = 0
+        i = (v[0]+snake[-1][0])%l
+        j = (v[1]+snake[-1][1])%h
 
-        # desenhar a matriz conforme o snake
-        for i,j in snake:
-            buffer[j][i] = 1
+        # Se ela acertar a própria cauda morre
+        if (i,j) in snake:
+            rodando = False
         
-        # desenhar retângulos conforme a matriz buffer
-        for i in range(32):
-            for j in range(40):
-                if buffer[i][j] == 1:
-                    pygame.draw.rect(tela,VERDE,(j*20,i*20,20,20))
+        snake.append((i,j))
+
+        # Colidindo
+        if maçã == (i,j):
+            comprimento += 1
+            pontos += 1
+            maçã = randint(0,l-1),randint(0,h-1)
+            while maçã in snake:
+                maçã = randint(0,l-1),randint(0,h-1)
+
+        # desenhar maçã e snake
+        x,y = maçã
+        pygame.draw.rect(tela,VERMELHO,(x*a,y*a,a,a))
+        for i,j in snake:
+            pygame.draw.rect(tela,VERDE,(i*a,j*a,a,a))
         
         # limita o tamanho do snake
-        if len(snake) > 4:
+        if len(snake) > comprimento:
             snake.pop(0)
+
+        # Atualiza o placar
+        mensage = f'Pontos: {pontos}'
+        placar = fonte.render(mensage,True,PRETO)
+        tela.blit(placar,(500,0))
 
         pygame.display.flip()
 
-    
     pygame.quit()
             
 

181a2dd4df93eafe1f2e0c4432256577206fad6c
orahcio <orahcio@gmail.com>
2024-04-10 19:49:05 -0300

    Instalado pgz, só consegui com o wheel, nada feito se instalar da fonte

--- flake.nix
+++ flake.nix
@@ -12,17 +12,39 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+      my_pyfxr = pkgs.python311Packages.buildPythonPackage rec {
+        # propagatedBuildInputs = with pkgs.python3Packages; [ setuptools ];
+        pname = "pyfxr";
+        version = "0.3.0";
+        format = "wheel";
+        url = "https://files.pythonhosted.org/packages/83/f9/68ef33dcdfef32e9e479d209371d11b84ef561f3369694142a6212863761/pyfxr-0.3.0-pp310-pypy310_pp73-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl";
+        src = pkgs.fetchPypi {
+          inherit pname version;
+          sha256 = "de997cb504f0e0fbe9a21f2174ea0a105a8748952521410e4691df7027876083";
+        };
+      };
+      my_pgzero = pkgs.python311Packages.buildPythonPackage rec {
+        propagatedBuildInputs = with pkgs.python311Packages; [ numpy pygame my_pyfxr ];
+        pname = "pgzero";
+        version = "1.2.1";
+        format = "wheel";
+        url = "https://files.pythonhosted.org/packages/2c/66/bc46c203802d47fa30a6caa92d13392274bcbebbb9ffcd0c5ed8030b3611/pgzero-1.2.1-py3-none-any.whl";
+        src = pkgs.fetchurl {
+          inherit url;
+          sha256 = "734e1de1a99804c2610f90aa419411fc2b31200b9d683b6c9fc710c7a8e36606";
+        };
+      };
     in rec {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
           # stdenv.cc.cc.lib
           # SDL2
           # xorg.libX11
-          (python3.withPackages(ps: with ps; [
+          (python311.withPackages(ps: with ps; [
             pip
-            jupyter
-            jupyterlab
+            ipykernel
             pygame
+            my_pgzero
           ]))
           pandoc
           # virtualenv

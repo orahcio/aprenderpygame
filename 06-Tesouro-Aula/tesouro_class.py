bf6f719df868916b36af2c091870bce25fc16c06
orahcio <orahcio@gmail.com>
2024-05-14 13:47:46 -0300

    Figuras redimensionadas e cortadas

--- 06-Tesouro-Aula/tesouro_class.py
+++ 06-Tesouro-Aula/tesouro_class.py
@@ -1,6 +1,35 @@
 from random import randint
 # from pgzhelper import *
 
+
+class Hero2(Actor):
+    def __init__(self, image, pos=None, anchor=None, vel=7, life=3, **kwargs):
+        super().__init__(image, pos, anchor, **kwargs)
+        self.vel = vel
+        self.life = life
+        self.score = 0
+        
+        
+    def update(self):
+        if keyboard.left or keyboard.a:
+            ship.x -= self.vel
+            if ship.left < 0: # Parede à esquerda
+                ship.left = 0
+        if keyboard.right or keyboard.d:
+            ship.x += self.vel
+            if ship.right > WIDTH: # Parede à direita`
+                ship.right = WIDTH
+    
+
+    def collide(self, item, items, c=1):
+        if self.colliderect(item):
+            if c>0: self.score += c
+            else: self.life += c
+            item.set_pos()
+            item.som()
+            items.remove(item)
+
+
 class Hero(Actor):
 
     vel = 7
@@ -18,13 +47,35 @@ class Hero(Actor):
                 ship.right = WIDTH
     
 
-    def collide(self, item, c=1):
+    def collide(self, item, items, c=1):
         if self.colliderect(item):
             if c>0: self.score += c
             else: self.life += c
             item.set_pos()
             item.som()
+            items.remove(item)
 
+
+
+class Item2(Actor):
+    global HEIGHT
+
+
+    def __init__(self, image, pos=None, anchor=None, vel=7, som=None, **kwargs):
+        super().__init__(image, pos, anchor, **kwargs)
+        self.vel = vel
+        self.midtop = randint(20,780), 0
+        self.som = som.play
+
+
+    def set_pos(self):
+        self.midtop = randint(20,780), 0
+
+    
+    def update(self):
+        self.y += self.vel
+        if self.bottom > HEIGHT:
+            self.midtop = randint(20,780), 0
 
 
 class Item(Actor):
@@ -44,22 +95,25 @@ class Item(Actor):
 
     def set_sound(self, som):
         self.som = som.play
+    
+
+    def create(self,som):
+        self.set_pos()
+        self.set_sound(som)
+
 
 
 WIDTH = 800
 HEIGHT = 600
 
-ship = Hero('ship', midbottom=(WIDTH/2,HEIGHT))
+ship = Hero2('ship', midbottom=(WIDTH/2,HEIGHT))
 
-coin = Item('coin')
-coin.set_pos()
-coin.set_sound(sounds.coleta)
+coins = [Item2('coin', som=sounds.coleta)]
 
-bomb = Item('bomb')
-bomb.set_pos()
-bomb.set_sound(sounds.explosao)
+bombs = [Item2('bomb', som=sounds.explosao)]
 
 game_over = False
+timeout = 0
 
 def draw():
     global game_over
@@ -67,8 +121,12 @@ def draw():
     if not game_over:
         screen.blit('background',(0,0))
         ship.draw()
-        coin.draw()
-        bomb.draw()
+
+        for coin in coins:
+            coin.draw()
+        for bomb in bombs:
+            bomb.draw()
+
         screen.draw.text(f'Pontos: {ship.score}',topleft=(0,0), fontname='pixel', color=(255,255,255), fontsize=15)
         screen.draw.text(f'Vidas: {ship.life}', topright=(800,0), fontname='pixel' ,color=(255,255,255), fontsize=15)
     
@@ -80,17 +138,32 @@ def draw():
 
 
 def update():
-    global game_over
+    global game_over, timeout
 
     if not game_over:
         ship.update()
 
-        coin.update()
-        bomb.update()
+        if timeout>100:
+            timeout = 0
+            coins.append(Item2('coin', som=sounds.coleta))
+            # coins[-1].create(sounds.coleta)
+            if len(bombs) < 3:
+                bombs.append(Item2('bomb', som=sounds.explosao))
+                # bombs[-1].create(sounds.explosao)
+            else:
+                bombs.pop(0)
+                print(len(bombs))
+       
 
-        ship.collide(coin)
-        
-        ship.collide(bomb, -1)
+        for coin in coins:
+            coin.update()
+            ship.collide(coin, coins)
+
+        for bomb in bombs:
+            bomb.update()
+            ship.collide(bomb, bombs, -1)
 
         if ship.life == 0:
             game_over = True
+        
+        timeout += 1
